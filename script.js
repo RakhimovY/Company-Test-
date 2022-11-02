43a8994b3058c3df191b08e338b76ed2d2f8151b
RakhimovY <rakhimov.y.hh@gmail.com>
2022-11-02 20:14:28 +0600

    +JS Script

--- script.js
+++ script.js
@@ -8,12 +8,10 @@ function getNextMonday() {
     nextMonday.setDate(nextMonday.getDate() + 1); // Adding 1 day
   } while (nextMonday.getDay() !== 1);
 
-  document.getElementById("Date").innerHTML = nextMonday.toDateString();
-  document.getElementById("Date1").innerHTML = nextMonday.toDateString();
-  document.getElementById("Date2").innerHTML = nextMonday.toDateString();
-  document.getElementById("Date3").innerHTML = nextMonday.toDateString();
-  document.getElementById("Date4").innerHTML = nextMonday.toDateString();
-  
-  return console.log(nextMonday.toDateString());
+  document.getElementById(
+    "Date"
+  ).innerHTML = `Распродажа до ${nextMonday.toLocaleDateString()}!`;
+
+  return console.log(nextMonday.toLocaleDateString());
 }
 getNextMonday();
